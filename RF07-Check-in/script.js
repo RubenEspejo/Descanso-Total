2ac5118b989aa9bbc7725b1cbc11650cbff6c23b
BenjaVodka <benjaminromerodiaz19@gmail.com>
2026-09-09 17:47:32 -0300

    He avanzado en el script del check in

---
+++ RF07-Check-in/script.js
@@ -0,0 +1,40 @@
+    const inputTexto = document.getElementById('codigo');
+    const boton = document.getElementById('btnValidar');
+    const parrafoResultado = document.getElementById('resultado');
+    const codigoValido = "ABCDFG";
+
+
+    const inputTexto2 = document.getElementById('btnRegistrar');
+    const parrafoResultado2 = document.getElementById('resultado2');
+
+
+    boton.addEventListener('click', function() {
+
+        const textoIngresado = inputTexto.value;
+
+        if (textoIngresado.trim() !== codigoValido){
+            parrafoResultado.textContent="No has ingresado un codigo valido, intentalo otra vez";
+            parrafoResultado.style.color = "red";
+        } else {
+            parrafoResultado.textContent = "Codigo valido";
+            parrafoResultado.style.color = "green";
+        }
+        
+    });
+
+    boton.addEventListener('click', function() {
+
+        const textoIngresado2 = inputTexto2.value;
+
+        if (textoIngresado2.trim() == codigoValido){
+            parrafoResultado2.textContent="Ingreso registrado exitosamente";
+            parrafoResultado2.style.color ="green";
+
+        }else{
+            parrafoResultado2.textContent="Este codigo ya está tomado o no está disponible, intente otra vez";
+            parrafoResultado2.style.color="red";
+        }
+    });
+
+
+
